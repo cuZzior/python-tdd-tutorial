f98450faa9f37c1a16af057b7cc1850972793123
cuZzior <piotr.necka@gmail.com>
2020-01-29 22:25:17 +0100

    First end-to-end test using unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,40 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Pioter dowiedział się o nowej to-do apce. Odpala strone.
+        self.browser.get('http://localhost:8000')
+
+        # Widzi 'To-Do' w tytule strony
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # Odrazu ma mozliwosc dodawania to-do listy
+
+        # Dodaje "Kup karme dla kota" bo kocha koty
+
+        # Klika enter i strone sie aktualizuje, widac w niej
+        # "1: Kup karme dla kota" w liscie to-do
+
+        # Wciaz widzi tekst proponujacy dodanie nowego to-do
+        # Dodaje "Nasyp tej karmy kotkowi"
+
+        # Po kliknieciu enter strona znowu sie aktualizuje i teraz widzi 2 punkty
+
+        # Pioter zastanawia sie czy strona zapamieta jej liste.
+        # Widzi ze strona wygenerowala specjalny URL ktory jest unikatowy.
+        # Dostaje rowniez wyjaśnienie tego.
+
+        # Wchodzi w ten link i widzi liste ktora wczesniej wprowadzila
+
+        # Usatysfakcjonowany idzie spac :)
+if __name__ == '__main__':
+    unittest.main()

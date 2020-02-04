4b0207ec4af2ea867ca981e40192295954205edc
cuZzior <piotr.necka@gmail.com>
2020-02-04 17:07:29 +0100

    Display table on page

--- functional_tests.py
+++ functional_tests.py
@@ -36,12 +36,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Kup karme dla kota' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Kup karme dla kota', [row.text for row in rows])
         # Wciaz widzi tekst proponujacy dodanie nowego to-do
         # Dodaje "Nasyp tej karmy kotkowi"
+        self.assertIn('2: Nasyp tej karmy kotkowi', [row.text for row in rows])
         self.fail('Finish the test!')
 
         # Po kliknieciu enter strona znowu sie aktualizuje i teraz widzi 2 punkty
